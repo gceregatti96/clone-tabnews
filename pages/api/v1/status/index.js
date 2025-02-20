732d1f5a160d6c13d7b1c493fe944bd7400ee051
Giancarlo Ceregatti <88408393+gceregatti96@users.noreply.github.com>
2025-02-20 15:12:36 +0000

    tests updates

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  //consulta ao banco de dados
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const dataBaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';"
+  );
+  const dataBaseOpenedConnectionsValue = dataBaseOpenedConnectionsResult.rows.length;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: dataBaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
